abd141757655ee8f60c4ff278f38d7abc5e8c71b
Sahabiya Sifat Siddique <48355912+sahabiya-sifat-sidique@users.noreply.github.com>
2020-02-29 23:19:53 +0530

    Create GetConnection.java

---
+++ src/service/GetConnection.java
@@ -0,0 +1,19 @@
+
+package service;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class GetConnection {
+   private static Connection con = null;
+   public static Connection getConnection(){
+     try{
+       Class.forName("com.mysql.jdbc.Driver");
+       con = DriverManager.getConnection("jdbc:mysql://localhost:3306/matrimonial-db","root", "");
+     }
+     catch(Exception e){
+        e.printStackTrace();
+     }
+     return con;
+   }
+}
